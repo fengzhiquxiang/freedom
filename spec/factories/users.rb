ae2d52213d0da0133a279f5b1a04f2a5dee3b973
zgl <fengzhiquxiang@gmail.com>
2015-03-06 12:21:23 +0800

    delete some file3

--- spec/factories/users.rb
+++
@@ -1,9 +0,0 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
-
-FactoryGirl.define do
-  factory :user do
-    name "MyString"
-    password "MyString"
-    password_digest "MyString"
-  end
-end
